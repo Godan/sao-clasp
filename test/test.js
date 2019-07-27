56483af6288bc09c508c6be97706b64b92bac8e3
godan <matsushita@godan.xyz>
2019-07-27 15:27:37 +0900

    add test case

--- test/test.js
+++ test/test.js
@@ -8,3 +8,55 @@ test('defaults', async t => {
   const stream = await sao.mock({ generator })
   t.true(stream.fileList.includes('src/index.ts'))
 })
+
+// check generate index.ts and check not generate index.js
+test('typeScript', async t => {
+  const mockPromptAnswers = { language: 'ts' }
+  const stream = await sao.mock({ generator }, mockPromptAnswers)
+
+  t.true(stream.fileList.includes('src/index.ts'))
+  t.false(stream.fileList.includes('src/index.js'))
+
+  // json syntax check
+  stream.readFile('package.json').then(text => {
+    t.notThrows(() => JSON.parse(text))
+  })
+})
+
+// check generate index.js and check not generate index.ts
+test('JavaScript(Legacy)', async t => {
+  const mockPromptAnswers = { language: 'js' }
+  const stream = await sao.mock({ generator }, mockPromptAnswers)
+
+  t.true(stream.fileList.includes('src/index.js'))
+  t.false(stream.fileList.includes('src/index.ts'))
+
+  // json syntax check
+  stream.readFile('package.json').then(text => {
+    t.notThrows(() => JSON.parse(text))
+  })
+})
+
+// check syntax pacage.json, if not use jest option 
+test('not use jest', async t => {
+  const mockPromptAnswers = { language: 'ts', jest: false }
+  const stream = await sao.mock({ generator }, mockPromptAnswers)
+
+  t.true(stream.fileList.includes('package.json'))
+  // json syntax check
+  stream.readFile('package.json').then(text => {
+    t.notThrows(() => JSON.parse(text))
+  })
+})
+
+// check syntax pacage.json, if not use lint option
+test('not use lint', async t => {
+  const mockPromptAnswers = { language: 'ts', lint: false }
+  const stream = await sao.mock({ generator }, mockPromptAnswers)
+
+  t.true(stream.fileList.includes('package.json'))
+  // json syntax check
+  stream.readFile('package.json').then(text => {
+    t.notThrows(() => JSON.parse(text))
+  })
+})
